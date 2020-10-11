a547693bddf475fc2dccdb5b1cf087453990db4a
Омельянов Дима <domelyanov@hotmail.com>
2020-10-11 21:46:31 +0300

    c

--- homework.js
+++ homework.js
@@ -757,23 +757,55 @@ mood : happy
 
 // !MODULE 5
 
-// Write code under this line
+// ? Задание 1
+// const Account = function (login, email) {
+//     this.login = login;
+//     this.email = email;
+//    Account.prototype.getInfo = function () {
+//       return (`login : ${this.login}, email:${this.email}`);
+//       };
+// };
 
-const Account = function (login, email) {
-    this.login = login;
-    this.email = email;
-    this.getInfo = function () {
-      return `login : ${this.login}, email: ${this.email} `;
-      };
-};
 
-console.log(typeof Account.prototype.getInfo);
+// console.log(typeof Account.prototype.getInfo);
+// // 'function'
+
+// const mango = new Account('Mangozedog', 'mango@dog.woof');
+// console.log(mango.getInfo());
+// // 'login : Mangozedog, email: mango@dog.woof'
+
+// const poly = new Account('Poly', 'poly@mail.com');
+// console.log(poly.getInfo());
+// // 'login : Poly, email: poly@mail.com'
+// ? Задание 2
+ // Write code under this line
+class User {
+    constructor(name, age, followers) {
+        this.name = name;
+        this.age = age;
+        this.followers = followers
+    }
+    getInfo = function () { }
+}
+
+
+ console.log(typeof User);
 // 'function'
 
-const mango = new Account('Mangozedog', 'mango@dog.woof');
-console.log(mango.getInfo());
-// 'login : Mangozedog, email: mango@dog.woof'
+ const mango = new User('Mango', 2, 20);
+ console.log(mango.getInfo()); 
+// 'User Mango is 2 years old and has 20 followers'
 
-const poly = new Account('Poly', 'poly@mail.com');
-console.log(poly.getInfo());
-// 'login : Poly, email: poly@mail.com'
+ console.log(typeof mango.getInfo); 
+// 'function'  
+
+ const poly = new User( 'Poly', 3, 17);
+ console.log(poly.getInfo());
+// 'User Poly is 3 years old and has 17 followers'
+
+// ? Задание 3
+
+// ? Задание 4
+
+// ? Задание 5
+
